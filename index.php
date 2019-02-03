eb4e27b8fc0aa7c9914cfd3664bfb9f97711e4c5
pavel.karachev <pavel.karachev@team.wrike.com>
2019-02-03 12:31:08 +0300

    Добавлеяет htmlspecialchars

--- index.php
+++ index.php
@@ -137,7 +137,7 @@ $tasks = [
                                     <label class="checkbox task__checkbox">
                                         <input class="checkbox__input visually-hidden task__checkbox" type="checkbox"
                                                value="1">
-                                        <span class="checkbox__text"><?= $task['name']; ?></span>
+                                        <span class="checkbox__text"><?= htmlspecialchars($task['name']); ?></span>
                                     </label>
                                 </td>
 
