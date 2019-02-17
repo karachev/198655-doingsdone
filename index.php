219f18dac8285da76573afde21cfc071292bb424
pavel.karachev <pavel.karachev@team.wrike.com>
2019-02-17 15:59:05 +0300

    Подключает sql к проекту

--- index.php
+++ index.php
@@ -1,6 +1,23 @@
 <?php
-require_once('functions.php');
-require_once('data.php');
+require_once('init.php');
+
+// код SQL-запроса
+$sql = 'SELECT name FROM project';
+//
+$result = mysqli_query($link, $sql);
+if ($result) {
+    $projects = mysqli_fetch_all($result, MYSQLI_ASSOC);
+} else {
+    print('На сайте ведутся технические работы');
+}
+
+$sql = 'SELECT * FROM task WHERE id = 1';
+
+if ($res = mysqli_query($link, $sql)) {
+    $tasks = mysqli_fetch_all($res, MYSQLI_ASSOC);
+} else {
+    print('На сайте ведутся технические работы22');
+}
 
 $page_content = include_template('index.php', [
     'tasks' => $tasks,
